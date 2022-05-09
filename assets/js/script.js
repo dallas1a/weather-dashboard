1244510ae4dc2f763f54e2464663110324b53850
dalla@MSI <dallas.archer.bailey@gmail.com>
2022-05-08 23:54:15 -0400

    added button to be able to search for a new city, stores previous searches and creates buttons for them

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,44 +1,46 @@
+//sets variable for the key to call api and variable for initial example city
 var key = "91b941255c14063d95fdc2a4aa5d6e7b";
 var city = "Hartford"
-
+//calls moment.js for time outputs
 var date = moment().format('dddd, MMMM Do YYYY');
 var dateTime = moment().format('YYYY-MM-DD HH:MM:SS')
 
 
 
 var currentCityToday = $('.currentCityToday')
-
+//get the weather for the day for the example pulling info from weather api calls
 function currentWeather(){
 	var retrieveCurrent = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${key}`;
 
 	$(currentCityToday).empty();
-
+//retrieves api information from the url using the city as the search criteria 
 	$.ajax({
 		url: retrieveCurrent,
 		method: 'GET',
+        //after retrieving it appends the cooresponding info to each api call
 	}).then(function (response) {
 		$('.cardTodayCityName').text(response.name);
 		$('.dateToday').text(date);
 		
 		$('.icons').attr('src', `https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`);
 		
-		var pEl = $('<p>').text(`Temperature: ${response.main.temp} °F`);
-		currentCityToday.append(pEl);
+		var pElTempCon = $('<p>').text(`Temperature: ${response.main.temp} °F`);
+		currentCityToday.append(pElTempCon);
 		
-		var pElTemp = $('<p>').text(`Feels Like: ${response.main.feels_like} °F`);
-		currentCityToday.append(pElTemp);
+		var pElFeel = $('<p>').text(`Feels Like: ${response.main.feels_like} °F`);
+		currentCityToday.append(pElFeel);
 		
-		var pElHumid = $('<p>').text(`Humidity: ${response.main.humidity} %`);
-		currentCityToday.append(pElHumid);
+		var pElHumidCon = $('<p>').text(`Humidity: ${response.main.humidity} %`);
+		currentCityToday.append(pElHumidCon);
 		
-		var pElWind = $('<p>').text(`Wind Speed: ${response.wind.speed} MPH`);
-		currentCityToday.append(pElWind);
+		var pElWindCon = $('<p>').text(`Wind Speed: ${response.wind.speed} MPH`);
+		currentCityToday.append(pElWindCon);
 		
 		
         var cityLon = response.coord.lon;
 		var cityLat = response.coord.lat;
 		
-
+//uses the latitude and longitude to get the uv index for the coordinates
 		var retrieveUvi = `https://api.openweathermap.org/data/2.5/onecall?lat=${cityLat}&lon=${cityLon}&exclude=hourly,daily,minutely&appid=${key}`;
 
 		$.ajax({
@@ -50,7 +52,7 @@ function currentWeather(){
 			var uvi = response.current.uvi;
 			pElUvi.append(uviSpan);
 			currentCityToday.append(pElUvi);
-			
+			//for a value of the given uvi it gives the uviSpan a class attribute that styles the warning color in the css
 			if (uvi >= 0 && uvi <= 2) {
 				uviSpan.attr('class', 'low');
 			} else if (uvi > 2 && uvi <= 5) {
@@ -64,10 +66,10 @@ function currentWeather(){
 			}
 		});
 	});
-	
+	collectUpcomingFive();
 };
 var upcomingFiveEl = $('.upcomingFive');
-
+//uses the weather api to create an array that gets the upcoming 5 day forecast for the city given
 function collectUpcomingFive() {
 	var getUpcomingFIveUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${key}`;
 
@@ -77,7 +79,7 @@ function collectUpcomingFive() {
 	}).then(function (response) {
 		var fiveDayArray = response.list;
 		var dailyUpcoming = [];
-		
+		//creates an object for the info for each days weather 
 		$.each(fiveDayArray, function (index, value) {
 			testObj = {
 				date: value.dt_txt.split(' ')[0],
@@ -92,7 +94,7 @@ function collectUpcomingFive() {
 				dailyUpcoming.push(testObj);
 			}
 		})
-		
+		//creates a card for each of the 5 days in the forecast appending all the info 
 		for (let i = 0; i < dailyUpcoming.length; i++) {
 
 			var divElCard = $('<div>');
@@ -116,22 +118,73 @@ function collectUpcomingFive() {
 			divElBody.append(divElIcon);
 
 			
-			var pElTemp = $('<p>').text(`Temperature: ${dailyUpcoming[i].temp} °F`);
-			divElBody.append(pElTemp);
+			var pElTempCon = $('<p>').text(`Temperature: ${dailyUpcoming[i].temp} °F`);
+			divElBody.append(pElTempCon);
 			
 			var pElFeel = $('<p>').text(`Feels Like: ${dailyUpcoming[i].feels_like} °F`);
 			divElBody.append(pElFeel);
 			
-			var pElHumid = $('<p>').text(`Humidity: ${dailyUpcoming[i].humidity} %`);
-			divElBody.append(pElHumid);
+			var pElHumidCon = $('<p>').text(`Humidity: ${dailyUpcoming[i].humidity} %`);
+			divElBody.append(pElHumidCon);
 		}
 	});
 };
+var previousSearch = [];
+//listens for a click on the search button, then uses the city inputted to output
+// that cities weather info for the day and 5 day forecast
+$('.search').on("click", function (event) {
+	event.preventDefault();
+	city = $(this).parent('.btnParam').siblings('.textVal').val().trim();
+	if (city === "") {
+		return;
+	};
+	previousSearch.push(city);
 
+	localStorage.setItem('city', JSON.stringify(previousSearch));
+	upcomingFiveEl.empty();
+	retrieveHistory();
+	currentWeather();
+});
 
+var fillHistoryEl = $('.previousSearch');
+//collects previously saved cities and turns them into buttons that can be used to re search that cities weather info 
+function retrieveHistory() {
+	fillHistoryEl.empty();
 
+	for (let i = 0; i < fillHistoryEl.length; i++) {
 
+		var rowEl = $('<row>');
+		var btnEl = $('<button>').text(`${previousSearch[i]}`)
 
+		rowEl.addClass('row previousCities');
+		btnEl.addClass('btn btn-outline-secondary previousCityBtn');
+		btnEl.attr('type', 'button');
 
-currentWeather();
-collectUpcomingFive();
+		fillHistoryEl.prepend(rowEl);
+		rowEl.append(btnEl);
+	} if (!city) {
+		return;
+	}
+	
+	$('.previousCityBtn').on("click", function (event) {
+		event.preventDefault();
+		city = $(this).text();
+		upcomingFiveEl.empty();
+		currentWeather();
+	});
+};
+
+//loads page with the example as the initial page, loads any previously searched cities from 
+//local storage into a list of clickable buttons
+function loadPage() {
+
+	var collectPrevious = JSON.parse(localStorage.getItem('city'));
+
+	if (collectPrevious !== null) {
+		previousSearch = collectPrevious
+	}
+	retrieveHistory();
+	currentWeather();
+};
+
+loadPage();
