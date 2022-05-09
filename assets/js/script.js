d342793866074b4aae71738413cc3755729f2c6c
dalla@MSI <dallas.archer.bailey@gmail.com>
2022-05-08 21:49:39 -0400

    added 5 day forecast for example city

--- assets/js/script.js
+++ assets/js/script.js
@@ -52,23 +52,86 @@ function currentWeather(){
 			currentCityToday.append(pElUvi);
 			
 			if (uvi >= 0 && uvi <= 2) {
-				uviSpan.attr('class', 'green');
+				uviSpan.attr('class', 'low');
 			} else if (uvi > 2 && uvi <= 5) {
-				uviSpan.attr("class", "yellow")
+				uviSpan.attr("class", "moderate")
 			} else if (uvi > 5 && uvi <= 7) {
-				uviSpan.attr("class", "orange")
+				uviSpan.attr("class", "high")
 			} else if (uvi > 7 && uvi <= 10) {
-				uviSpan.attr("class", "red")
+				uviSpan.attr("class", "very-high")
 			} else {
-				uviSpan.attr("class", "purple")
+				uviSpan.attr("class", "extreme")
 			}
 		});
 	});
 	
 };
+var upcomingFiveEl = $('.upcomingFive');
+
+function collectUpcomingFive() {
+	var getUpcomingFIveUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${key}`;
+
+	$.ajax({
+		url: getUpcomingFIveUrl,
+		method: 'GET',
+	}).then(function (response) {
+		var fiveDayArray = response.list;
+		var dailyUpcoming = [];
+		
+		$.each(fiveDayArray, function (index, value) {
+			testObj = {
+				date: value.dt_txt.split(' ')[0],
+				time: value.dt_txt.split(' ')[1],
+				temp: value.main.temp,
+				feels_like: value.main.feels_like,
+				icon: value.weather[0].icon,
+				humidity: value.main.humidity
+			}
+
+			if (value.dt_txt.split(' ')[1] === "12:00:00") {
+				dailyUpcoming.push(testObj);
+			}
+		})
+		
+		for (let i = 0; i < dailyUpcoming.length; i++) {
+
+			var divElCard = $('<div>');
+			divElCard.attr('class', 'card text-white bg-primary mb-3  cardOne');
+			divElCard.attr('style', 'max-width: 200px;');
+			upcomingFiveEl.append(divElCard);
+
+			var divElHeader = $('<div>');
+			divElHeader.attr('class', 'card-header')
+			var day = moment(`${dailyUpcoming[i].date}`).format('dddd, MM-DD-YY');
+			divElHeader.text(day);
+			divElCard.append(divElHeader)
+
+			var divElBody = $('<div>');
+			divElBody.attr('class', 'card-body');
+			divElCard.append(divElBody);
+
+			var divElIcon = $('<img>');
+			divElIcon.attr('class', 'icons');
+			divElIcon.attr('src', `https://openweathermap.org/img/wn/${dailyUpcoming[i].icon}@2x.png`);
+			divElBody.append(divElIcon);
+
+			
+			var pElTemp = $('<p>').text(`Temperature: ${dailyUpcoming[i].temp} °F`);
+			divElBody.append(pElTemp);
+			
+			var pElFeel = $('<p>').text(`Feels Like: ${dailyUpcoming[i].feels_like} °F`);
+			divElBody.append(pElFeel);
+			
+			var pElHumid = $('<p>').text(`Humidity: ${dailyUpcoming[i].humidity} %`);
+			divElBody.append(pElHumid);
+		}
+	});
+};
 
 
-//function collectUpcomingFive(){}
 
 
-    currentWeather();
+
+
+currentWeather();
+collectUpcomingFive();
